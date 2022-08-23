4a454b5f030c3c3384172dbd54407fb566f69492
pankajjossy1 <50371645+pankajjossy1@users.noreply.github.com>
2022-08-23 22:23:00 +0530

    Update leapYear.js

--- leapYear.js
+++ leapYear.js
@@ -1,1 +1,4 @@
-//Leap Year Program 
+//Javascript code to check a given year is a Leap year or Not a leap Yar 
+var year = 2022;
+((year % 4 == 0 && year % 100 != 0) || (year % 400 ==0))  ? console.log("leap") : console.log("Not leap");
+
