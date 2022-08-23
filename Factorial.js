9ef0d2f942b4f3eabe6ea976db66386ce2317aea
pankajjossy1 <50371645+pankajjossy1@users.noreply.github.com>
2022-08-23 22:48:57 +0530

    Create Factorial.js

---
+++ Factorial.js
@@ -0,0 +1,6 @@
+// Javascript program to find the factorial of a number
+var num = 5, i, factorial=1;
+for (i=1; i<=num; i++){
+    factorial = factorial * i;
+}
+console.log(`Factorial of ${num} = ${factorial}`);
