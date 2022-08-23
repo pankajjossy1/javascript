4e4cb7dbfd90471176050fbcfa056b7fd0fb2155
pankajjossy1 <50371645+pankajjossy1@users.noreply.github.com>
2022-08-23 22:43:09 +0530

    Update tempConversion.js

--- tempConversion.js
+++ tempConversion.js
@@ -1,1 +1,11 @@
 //Celcius to Farenheit and vice versa
+
+//javascript code to convert celcius to fahrenheit and vice-versa
+
+var celciusTemp = 30, farTemp=130;
+var celciusToFar, farToCelcius;
+celciusToFar = (5 / 9) * (farTemp - 32);
+farToCelcius = (celciusTemp * 9) / 5 + 32;
+console.log(`Celcius Temperature = ${celciusToFar}`);
+console.log(`Farenheit Temperature ${farToCelcius}`);
+
